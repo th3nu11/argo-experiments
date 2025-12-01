49d9782793c111f1de59b99e4d9a04dd0503f42b
carlo.ballabio <carlo.ballabio@intre.it>
2025-12-01 18:09:48 +0100

    Add /work route to calculate and return computation duration and pod ID

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const podId = process.env.HOSTNAME || "unknown";
+
+const loopCount = 10_000_000;
 
 app.get('/hello1', (req, res) => {
-  res.send('Hello World 1!');
+    res.send('Hello World 1!');
 });
 
-app.get('/hello2', (req, res) => {
-  res.send('Hello World 2!!');
+app.get('/work', (req, res) => {
+    const start = process.hrtime.bigint();
+
+    let x = 0;
+    for (let i = 0; i < loopCount; i++) {
+        x += Math.sqrt(i * Math.random());
+    }
+    const end = process.hrtime.bigint();
+    const durationMs = Number(end - start) / 1_000_000;
+
+    res.json({
+        pod: podId,
+        result: x,
+        durationMs: durationMs
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
